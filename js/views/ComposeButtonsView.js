b7b14c3568aa1c61fad994380f3af75b7f9e7765
artemij-nemec <artem@chesnokov.pp.ua>
2020-03-13 14:16:34 +0300

    The SDSE is not allowed to execute twice

--- js/views/ComposeButtonsView.js
+++ js/views/ComposeButtonsView.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var
+	ko = require('knockout'),
+
 	Popups = require('%PathToCoreWebclientModule%/js/Popups.js'),
 	SelfDestructingEncryptedMessagePopup = require('modules/%ModuleName%/js/popups/SelfDestructingEncryptedMessagePopup.js')
 ;
@@ -10,6 +12,7 @@ var
  */
 function CComposeButtonsView()
 {
+	this.pgpSecured = ko.observable(false);
 }
 
 CComposeButtonsView.prototype.ViewTemplate = '%ModuleName%_ComposeButtonsView';
@@ -29,10 +32,60 @@ CComposeButtonsView.prototype.ViewTemplate = '%ModuleName%_ComposeButtonsView';
  * @param {Function} oCompose.setHtmlTextMode Sets html text mode switched on.
  * @param {Function} oCompose.setHtmlText Sets html text to html editor.
  * @param {Function} oCompose.undoHtml Undo last changes in html editor.
+ * 
+ * @param oCompose.koTextChange Triggered on changing text in compose
+ * 
  */
 CComposeButtonsView.prototype.assignComposeExtInterface = function (oCompose)
 {
 	this.oCompose = oCompose;
+	this.oCompose.koTextChange.subscribe(function () {
+		var sPlainText = oCompose.getPlainText();
+		if (!oCompose.isHtml()) {
+			this.pgpSecured(sPlainText.indexOf('-----BEGIN PGP MESSAGE-----') !== -1);
+		}
+	}, this);
+};
+
+/**
+ * @param {Object} oParameters
+ */
+CComposeButtonsView.prototype.doAfterApplyingMainTabParameters = function (oParameters)
+{
+	if (oParameters.OpenPgp)
+	{
+		this.pgpSecured(oParameters.OpenPgp.Secured);
+	}
+};
+
+/**
+ * @param {Object} oParameters
+ */
+CComposeButtonsView.prototype.doAfterPreparingMainTabParameters = function (oParameters)
+{
+	oParameters.OpenPgp = {
+		Secured: this.pgpSecured()
+	};
+};
+
+/**
+ * Receives message properties that are displayed when opening the compose popup.
+ * 
+ * @param {Object} oMessageProps Receiving message properties.
+ * @param {Boolean} oMessageProps.bDraft **true** if message was opened from drafts folder.
+ * @param {Boolean} oMessageProps.bPlain **true** if opened for compose message if plain.
+ * @param {String} oMessageProps.sRawText Raw plain text of opened for compose message.
+ */
+CComposeButtonsView.prototype.doAfterPopulatingMessage = function (oMessageProps)
+{
+	if (oMessageProps.bPlain)
+	{
+		this.pgpSecured(oMessageProps.sRawText.indexOf('-----BEGIN PGP MESSAGE-----') !== -1);
+	}
+	else
+	{
+		this.pgpSecured(false);
+	}
 };
 
 CComposeButtonsView.prototype.send = function ()
@@ -52,7 +105,7 @@ CComposeButtonsView.prototype.send = function ()
  */
 CComposeButtonsView.prototype.isEnableSending = function ()
 {
-	return this.oCompose;
+	return this.oCompose && !this.pgpSecured();
 };
 
 module.exports = new CComposeButtonsView();
